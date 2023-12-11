8212df14a902b4b55f255961129e1c5c056fec2e
Sandeep <sandeep@StudyMonks-MacBook-Pro.local>
2023-12-11 17:37:02 +0530

    add random

--- src/index.js
+++ src/index.js
@@ -18,46 +18,50 @@ app.use(express.static("public"));
 app.post("/", async (req, res) => {
   try {
     const csvPath = path.join(__dirname, "merged_data.csv");
-  
+
     const { text } = req.body;
 
     const csvData = await csv().fromFile(csvPath);
 
-   
     const inputWords = text.split(/\s+/);
-   const extractedWords=[]
-    
-    csvData.map((items,id)=>{
-    //   console.log(items.extracted_x)
-       inputWords.forEach((word)=>{
-        
-        if(items.extracted_x.includes(word)){
-            extractedWords.push({
-                prerequisitetaxonomy:items.prerequisitetaxonomy,
-            })
+    const extractedWords = [];
+
+    csvData.map((items, id) => {
+      //   console.log(items.extracted_x)
+      inputWords.forEach((word) => {
+        if (items.extracted_x.includes(word)) {
+          extractedWords.push({
+            prerequisitetaxonomy: items.prerequisitetaxonomy,
+          });
         }
-       })
-    })
+      });
+    });
 
-
-    
     const removeDuplicates = (arr, key) => {
-        const seen = new Set();
-        return arr.filter((obj) => {
-          const value = obj[key];
-          if (!seen.has(value)) {
-            seen.add(value);
-            return true;
-          }
-          return false;
-        });
-      };
+      const seen = new Set();
+      return arr.filter((obj) => {
+        const value = obj[key];
+        if (!seen.has(value)) {
+          seen.add(value);
+          return true;
+        }
+        return false;
+      });
+    };
 
-      const resultArray = removeDuplicates(extractedWords, "prerequisitetaxonomy");
-      
-      
+    var resultArray = removeDuplicates(extractedWords, "prerequisitetaxonomy");
 
-    res.json({ result: resultArray.slice(0,2)});
+    var randomIndex1 = Math.floor(Math.random() * resultArray.length);
+    var randomIndex2 = Math.floor(Math.random() * resultArray.length);
+
+    var newArray = resultArray.slice(randomIndex1, randomIndex2);
+    if (newArray.length == 0) {
+      const randomNumber11 = Math.floor(Math.random() * 2);  
+      const randomNumber22 = Math.floor(Math.random() * 2);
+      newArray = resultArray.slice(randomNumber11, randomNumber22);
+    }
+
+    res.json({ result: newArray });
   } catch (error) {
     console.log("ERRR in handling files", error);
   }
